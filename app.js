b77773bfdad1a7108f5c1191462011ebf604d273
Margo Schaedel <margoschaedel@gmail.com>
2017-12-08 14:59:17 -0700

    Update query to select multiple fields

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const os = require('os');
 const bodyParser = require('body-parser');
 const app = express();
-
 const influx = new Influx.InfluxDB('http://127.0.0.1:8086/telegraf');
 
 app.use(bodyParser.json());
@@ -29,12 +28,13 @@ app.get('/', (request, response) => {
 
 app.get('/api/v1/usage', (request, response) => {
   influx.query(`
-    select mean("usage_user") as "mean_usage_user" from cpu
+    select mean("usage_user") as "mean_usage_user",
+    mean("usage_system") as "mean_usage_system" from cpu
     where time > now() - 1h and
     host = ${Influx.escape.stringLit(os.hostname())}
     group by time(10s)
     order by time desc
-    limit 20
+    limit 200
     `)
     .then(result => response.status(200).json(result))
     .catch(error => response.status(500).json({ error }));
--- public/scripts.js
+++ public/scripts.js
@@ -11,21 +11,26 @@ const loadData = () => {
       const unpackData = (arr, key) => {
         return arr.map(obj => obj[key])
       }
-      const data = [
-        {
-          type: 'scatter',
-          mode: 'lines',
-          name: 'Computer Usage',
-          x: unpackData(parsedResponse, 'time'),
-          y: unpackData(parsedResponse, 'mean_usage_user'),
-          line: {color: '#17BECF'}
-        }
-      ];
-
+      const firstTrace = {
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Mean User Usage',
+        x: unpackData(parsedResponse, 'time'),
+        y: unpackData(parsedResponse, 'mean_usage_user'),
+        line: {color: '#17BECF'}
+      }
+      const secondTrace = {
+        type: "scatter",
+        mode: "lines",
+        name: 'Mean System Usage',
+        x: unpackData(parsedResponse, 'time'),
+        y: unpackData(parsedResponse, 'mean_usage_system'),
+        line: {color: '#7F7F7F'}
+      }
+      const data = [firstTrace, secondTrace];
       const layout = {
-        title: 'My Time Series',
+        title: 'Local CPU Usage',
       };
-
       return Plotly.newPlot('graphs-container', data, layout);
     })
     .catch( error => console.log(error) );
